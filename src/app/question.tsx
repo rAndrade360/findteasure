dc677b629d1715b3303bcfad907e6e77bb9c8803
Renan de Andrade <renan.andrade@g.globo>
2024-12-08 01:00:41 -0300

    set hint after 2 minutes

--- src/app/question.tsx
+++ src/app/question.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { QuestionModel } from "./page";
 import { Form } from "./form";
 import { H2 } from "./h2";
+import { useEffect, useState } from "react";
 
 
 export const metadata: Metadata = {
@@ -20,11 +21,20 @@ interface QuestionProps {
 
 export const Question: React.FC<QuestionProps> = (props) => {
 
+  const [showHints, setShowHints] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowHints(true)
+    }, 120000)
+  })
+
   return (
     <div className="flex gap-4 items-center flex-col sm:flex-col">
       <H2>Responda a pergunta para desbloquer a charada!</H2>
       <p>{props.question && props.question.text}</p>
       <Form value={props.answer} setValue={props.setAnswer} submitForm={props.submitQuestionResponse} />
+      {showHints && <p>Dica: {props.question && props.question.hints[0]}</p>}
     </div>
   );
 }
